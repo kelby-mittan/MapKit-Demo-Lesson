2c135febd87a7e8262cde0100c4f17481ef9c640
Kelby Mittan <kelbymittan@pursuit.org>
2020-02-24 10:22:30 -0500

    Initial Commit

---
+++ MapKit-Demo/MapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MapKit-Demo
+//
+//  Created by Kelby Mittan on 2/24/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MapKit-DemoTests/MapKit_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  MapKit_DemoTests.swift
+//  MapKit-DemoTests
+//
+//  Created by Kelby Mittan on 2/24/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import XCTest
+@testable import MapKit_Demo
+
+class MapKit_DemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
